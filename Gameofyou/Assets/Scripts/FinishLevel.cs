4811689cbbc5e55fd4a89b9a1487a4f0ba87bd5f
Ahmet Ozan Özgür <aozanozgur@hotmail.com>
2023-12-20 21:00:32 +0300

    Game Mechanics 10
    Finish blocker(Karakter assetinin içinde), Pause

--- Gameofyou/Assets/Scripts/FinishLevel.cs
+++ Gameofyou/Assets/Scripts/FinishLevel.cs
@@ -15,9 +15,12 @@ public class FinishLevel : MonoBehaviour
     public int timeCalc;
     public int scoreCalc;
     public int totalScored;
+    public GameObject levelBlocker;
 
     void OnTriggerEnter()
     {
+        levelBlocker.SetActive(true);
+        levelBlocker.transform.parent = null;
         timeCalc = GlobalTimer.extendScore * 100;
         timeLeft.GetComponent<TMP_Text>().text = "Time Left: " + GlobalTimer.extendScore + " x 100";
         theScore.GetComponent<TMP_Text>().text = "Score: " + GlobalScore.currentScore;
--- Gameofyou/Assets/Scripts/GemRotate.cs
+++ Gameofyou/Assets/Scripts/GemRotate.cs
@@ -8,6 +8,6 @@ public class GemRotate : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, rotateSpeed, 0, Space.World);
+        transform.Rotate(0, rotateSpeed * Time.timeScale, 0, Space.World);
     }
 }
